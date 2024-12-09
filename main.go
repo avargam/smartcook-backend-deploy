bef44f3f23ea8edf8a117fc209119393cf53cfc3
Alonso Vargas <avargam@uc.cl>
2024-12-09 18:43:31 -0300

    minor fix

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ var recipeHistory []Recipe
 var latestResponse Recipe
 
 func enableCors(w *http.ResponseWriter, r *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "https://smartcook-frontend.onrender.com")
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, mode")
 }
