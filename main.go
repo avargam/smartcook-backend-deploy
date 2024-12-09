389d0fd4e18d181c713529aae2c138b41b6af6a2
Alonso Vargas <avargam@uc.cl>
2024-12-09 19:23:21 -0300

    minor fix

--- main.go
+++ main.go
@@ -54,7 +54,8 @@ func enableCors(w *http.ResponseWriter, r *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	(*w).Header().Set("Access-Control-Allow-Headers", "*")
-
+	(*w).Header().Set("Content-Type", "application/json")
+	
 	if r.Method == "OPTIONS" {
 		return
 	}
