f45fefa50c2d58ef4bc390097ed0546362c35166
Alonso Vargas <avargam@uc.cl>
2024-12-09 19:04:17 -0300

    minor fix

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ var latestResponse Recipe
 func enableCors(w *http.ResponseWriter, r *http.Request) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, mode")
+	(*w).Header().Set("Access-Control-Allow-Headers", "*")
 
 	if r.Method == "OPTIONS" {
 		return
